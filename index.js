8bb534c5f4ba29946a210076e14a5c4599ee64c8
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-03-04 19:03:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function nowServing(katzDeli) {
 function currentLine(katzDeliLine){
   let i=0;
   while (i < katzDeli.length){
-    line.push(' ' + [i+1] + '. ' + katzDeli[i])}
+    line.push(' ' + [i+1] + '. ' + katzDeli[i])
+  }
     if (katzDeli.length === 0){
       return 'The line is currently empty.';
     }else{
